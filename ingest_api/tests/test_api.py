a79dbdfe2ed69546c30aafc80a46825244cb3c11
Влад Шелест <sv@corp2.eu>
2026-01-04 21:26:19 +0000

    added pytest and change in workflow

---
+++ ingest_api/tests/test_api.py
@@ -0,0 +1,32 @@
+import json
+
+from httpx import AsyncClient
+
+from app_config.config import settings
+from ingest_api.main import app
+
+
+async def test_track_endpoint_integration(client):
+    """
+    Інтеграційний тест: API -> Redis
+    """
+    test_event = {
+        "event": "test_ci",
+        "type": "custom",
+        "service": "test_service",
+        "source": "pytest",
+        "user_id": "user_1",
+        "timestamp": "2026-01-04T12:00:00Z",
+        "payload": {"key": "value"},
+    }
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post("/ingest/track", json=test_event)
+
+    assert response.status_code == 202
+    assert response.json()["status"] == "accepted"
+
+    raw_data = await client.rpop(settings.REDIS_QUEUE_KEY)
+    assert raw_data is not None
+
+    data = json.loads(raw_data)
+    assert data["event"] == "test_ci"
